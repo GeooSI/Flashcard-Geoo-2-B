306fe4f0f82842e7caec57e1b41965ed02f0d03c
Geovana Santos Ivatiuk <133694285+GeooSI@users.noreply.github.com>
2024-11-05 09:57:02 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'O JavaScript é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual é a raiz quadrada de 144?',
+    'A raiz quadrada de 144 é 12'
+)
